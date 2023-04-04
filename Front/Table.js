c7356a1454b77db878074ec497ddd051efaf0d8d
s1zzer <chepko_daniil@mail.ru>
2023-04-05 00:03:57 +0300

    Fix

--- Front/Table.js
+++ Front/Table.js
@@ -41,3 +41,82 @@ xhr.onload = function() {
 
 // Отправка запроса на сервер
 xhr.send();
+
+
+// Получение ссылки на форму
+const sortForm = document.getElementById("sort-form");
+
+// Получение ссылки на список выбора сортировки
+const sortSelect = document.getElementById("sort-select");
+
+// Добавление обработчика события на отправку формы
+sortForm.addEventListener("submit", function(event) {
+  event.preventDefault(); // Отмена отправки формы
+
+  const sortValue = sortSelect.value; // Получение значения выбранного параметра сортировки
+
+  // Выбор функции сортировки в зависимости от выбранного параметра
+  if (sortValue === "wins-asc") {
+    sortAscWins();
+  } else if (sortValue === "wins-desc") {
+    sortDescWins();
+  }
+});
+
+// Функция сортировки по возрастанию для колонки "Количество побед в тендерах"
+function sortAscWins() {
+  // Получение индекса колонки "Количество побед в тендерах"
+  const index = 10;
+
+  // Сортировка ячеек колонки по возрастанию
+  cells[index].sort(function(a, b) {
+    const aValue = parseInt(a.textContent);
+    const bValue = parseInt(b.textContent);
+    if (isNaN(aValue)) {
+      return 1;
+    } else if (isNaN(bValue)) {
+      return -1;
+    } else {
+      return aValue - bValue;
+    }
+  });
+
+  // Перестановка ячеек таблицы в соответствии с новым порядком сортировки
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+    const newRow = document.createElement("tr");
+    for (let j = 0; j < cells.length; j++) {
+      newRow.appendChild(cells[j][i].cloneNode(true));
+    }
+    table.appendChild(newRow);
+  }
+}
+
+// Функция сортировки по убыванию для колонки "Количество побед в тендерах"
+function sortDescWins() {
+  // Получение индекса колонки "Количество побед в тендерах"
+  const index = 10;
+
+  // Сортировка ячеек колонки по убыванию
+  cells[index].sort(function(a, b) {
+    const aValue = parseInt(a.textContent);
+    const bValue = parseInt(b.textContent);
+    if (isNaN(aValue)) {
+      return -1;
+    } else if (isNaN(bValue)) {
+      return 1;
+    } else {
+      return bValue - aValue;
+    }
+  });
+
+  // Перестановка ячеек таблицы в соответствии с новым порядком сортировки
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+    const newRow = document.createElement("tr");
+    for (let j = 0; j < cells.length; j++) {
+      newRow.appendChild(cells[j][i].cloneNode(true));
+    }
+    table.appendChild(newRow);
+  }
+}
