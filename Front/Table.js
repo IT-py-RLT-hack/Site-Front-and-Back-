f2b4fa69dcf8c2f3adafda709907a1c253f658b3
s1zzer <chepko_daniil@mail.ru>
2023-04-06 14:57:07 +0300

    Fix

--- Front/Table.js
+++ Front/Table.js
@@ -21,7 +21,6 @@ const columnIndexes = {
   geo_lon: 16,
 };
 
-
 // Создание объекта XMLHttpRequest
 const xhr = new XMLHttpRequest();
 
@@ -52,6 +51,7 @@ xhr.onload = function() {
         const row = document.createElement("tr");
         for (let j = 0; j < cells.length; j++) {
             const cell = document.createElement("td");
+            cell.dataset.column = Object.keys(columnIndexes)[j];
             const cellText = document.createTextNode(cells[j]);
             cell.appendChild(cellText);
             row.appendChild(cell);
@@ -63,72 +63,9 @@ xhr.onload = function() {
 // Отправка запроса на сервер
 xhr.send();
 
-// Получение ссылки на форму
-const sortForm = document.getElementById("sort-form");
-
-// Получение ссылки на список выбора сортировки
-const sortSelect = document.getElementById("sort-select");
-
-// Добавление обработчика события на отправку формы
-sortForm.addEventListener("submit", function(event) {
-  event.preventDefault(); // Отмена отправки формы
-
-  const sortValue = sortSelect.value; // Получение значения выбранного параметра сортировки
-
-  // Выбор функции сортировки в зависимости от выбранного параметра
-  if (sortValue === "wins-asc") {
-    sortAscWins();
-  } else if (sortValue === "wins-desc") {
-    sortDescWins();
-  }
-});
 
 // Получение всех строк таблицы
 const rows = Array.from(tableData.getElementsByTagName("tr"));
 
-// Функция сортировки по возрастанию для колонки "Количество побед в тендерах"
-function sortAscWins() {
-  const sortValue = sortSelect.value;
-  const index = columnIndexes[sortValue.split('-')[0]];
-  const rows = Array.from(tableData.getElementsByTagName("tr")); // перемещаем сюда
-
-  rows.sort(function(a, b) {
-    const aValue = a.cells[index].textContent;
-    const bValue = b.cells[index].textContent;
-
-    if (aValue < bValue) {
-      return -1;
-    }
-    if (aValue > bValue) {
-      return 1;
-    }
-    return 0;
-  });
-
-  for (let i = 0; i < rows.length; i++) {
-    tableData.appendChild(rows[i]);
-  }
-}
-
-function sortDescWins() {
-  const sortValue = sortSelect.value;
-  const index = columnIndexes[sortValue.split('-')[0]];
-  const rows = Array.from(tableData.getElementsByTagName("tr")); // перемещаем сюда
-
-  rows.sort(function(a, b) {
-    const aValue = a.cells[index].textContent;
-    const bValue = b.cells[index].textContent;
-
-    if (aValue < bValue) {
-      return 1;
-    }
-    if (aValue > bValue) {
-      return -1;
-    }
-    return 0;
-  });
-
-  for (let i = 0; i < rows.length; i++) {
-    tableData.appendChild(rows[i]);
-  }
-}
+// Получение всех заголовков столбцов таблицы
+const headers = Array.from(tableData.getElementsByTagName("th"));
