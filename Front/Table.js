a07a57f94b4b5080ff1956697e43bc85aa46a9be
s1zzer <chepko_daniil@mail.ru>
2023-04-07 23:04:03 +0300

    Загрузка стала прогрессивной

--- Front/Table.js
+++ Front/Table.js
@@ -1,24 +1,20 @@
-// Получение ссылки на таблицу
 const tableData = document.getElementById("table-data");
+const pageSize = 100; // количество строк, загружаемых за один раз
+let currentPage = 0; // текущая страница
 
 const columnIndexes = {
-  inn: 0,
-  name: 1,
-  activity: 2,
-  city: 3,
-  create_date: 4,
-  reg_date: 5,
-  last_tender_date: 6,
-  contract_value: 7,
-  staff: 8,
-  ban: 9,
-  bidding_num: 10,
-  wins_num: 11,
-  not_executed_contracts: 12,
-  terminated_contracts: 13,
-  likelihood_of_problems: 14,
-  geo_lat: 15,
-  geo_lon: 16,
+  avg_staff_qty: 1,
+  procedure_qty: 2,
+  win_qty: 3,
+  exclude_date: 4,
+  msp_type: 5,
+  msp_category: 6,
+  usn_simlified_tax_system: 7,
+  envd_imputed_tax: 8,
+  amount_due: 9,
+  okopf_code: 10,
+  okfs_code: 11,
+  oktmo_reg_code: 12,
 };
 
 // Создание объекта XMLHttpRequest
@@ -35,87 +31,47 @@ xhr.onload = function() {
   // Разбиение CSV-файла на строки
   const lines = xhr.responseText.split("\n");
 
-  // Заполнение таблицы данными
-  for (let i = 0; i < lines.length; i++) {
-    // Удаляем пробелы и переводы строк из начала и конца строки
-    const line = lines[i].trim();
+  // Отображение первой страницы таблицы
+  renderTable(lines.slice(1, pageSize + 1));
 
-    // Разбиваем строку на ячейки, используя разделитель ","
-    const cells = line.split(",");
-
-    // Пропускаем первую строку, если она содержит заголовки
-    if (i === 0 && cells[0].toLowerCase() === "inn") {
-      continue;
+  // Добавление обработчика прокрутки страницы
+  window.addEventListener("scroll", handleScroll);
+  
+  function handleScroll() {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    
+    // Проверка, достигли ли мы конца страницы
+    if (scrollTop + clientHeight >= scrollHeight) {
+      // Загрузка следующей страницы таблицы
+      currentPage++;
+      const startIndex = currentPage * pageSize + 1;
+      const endIndex = startIndex + pageSize;
+      const rows = lines.slice(startIndex, endIndex);
+      renderTable(rows);
     }
+  }
+  
+  function renderTable(rows) {
+    // Заполнение таблицы данными
+    for (let i = 0; i < rows.length; i++) {
+      // Удаляем пробелы и переводы строк из начала и конца строки
+      const line = rows[i].trim();
 
-    const row = document.createElement("tr");
-    for (let j = 0; j < cells.length; j++) {
-      const cell = document.createElement("td");
-      cell.dataset.column = Object.keys(columnIndexes)[j];
-      const cellText = document.createTextNode(cells[j]);
-      cell.appendChild(cellText);
-      row.appendChild(cell);
+      // Разбиваем строку на ячейки, используя разделитель ","
+      const cells = line.split(",");
+
+      const row = document.createElement("tr");
+      for (let j = 1; j < cells.length; j++) {
+        const cell = document.createElement("td");
+        cell.dataset.column = Object.keys(columnIndexes)[j - 1];
+        const cellText = document.createTextNode(cells[j]);
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+      }
+      tableData.appendChild(row);
     }
-    tableData.appendChild(row);
-  }
-
-  // Получение всех строк таблицы
-  const rows = Array.from(tableData.getElementsByTagName("tr"));
-
-  // Получение всех заголовков столбцов таблицы
-  const headers = Array.from(document.querySelectorAll("th"));
-
-  // Получение заголовка столбца ИНН
-  const innHeader = headers.find(header => header.dataset.column === 'inn');
-
-  if (innHeader) {
-    // Добавление обработчика события клика на заголовок столбца ИНН
-    innHeader.addEventListener("click", function() {
-      // Сортировка строк таблицы по значению в ячейке столбца ИНН
-      rows.sort((a, b) => {
-        const aValue = a.cells[columnIndexes.inn].innerText;
-        const bValue = b.cells[columnIndexes.inn].innerText;
-        return aValue.localeCompare(bValue);
-      });
-
-      // Удаление всех строк из таблицы
-      tableData.innerHTML = "";
-
-      // Добавление отсортированных строк в таблицу
-      rows.forEach(row => {
-        tableData.appendChild(row);
-      });
-    });
-  } else {
-    console.error('Заголовок столбца ИНН не найден');
-  }
-
-    // Получение заголовка столбца Количество сотрудников
-  const staffHeader = headers.find(header => header.dataset.column === 'staff');
-
-  if (staffHeader) {
-    // Получение всех ячеек столбца Количество сотрудников
-    const staffCells = Array.from(tableData.querySelectorAll(`[data-column="${staffHeader.dataset.column}"]`));
-
-    // Добавление переключателя на страницу
-    const toggleStaff = document.createElement("input");
-    toggleStaff.type = "checkbox";
-    toggleStaff.checked = true;
-    toggleStaff.addEventListener("change", function() {
-      // Изменение отображения ячеек столбца Количество сотрудников
-      const display = this.checked ? "" : "none";
-      staffCells.forEach(cell => {
-        cell.style.display = display;
-      });
-    });
-
-    // Добавление переключателя в заголовок столбца Количество сотрудников
-    staffHeader.appendChild(toggleStaff);
-  } else {
-    console.error('Заголовок столбца Количество сотрудников не найден');
   }
 };
 
 // Отправка запроса на сервер
 xhr.send();
-
--- Front/script.js
+++ Front/script.js
@@ -1,20 +1,50 @@
-// Загрузка CSV-файла и вывод названий компаний
 Papa.parse("data.csv", {
-  download: true,
-  header: true,
-  delimiter: ",",
-  dynamicTyping: true,
-  complete: function(results) {
-    var companies = results.data;
-    var companyList = document.getElementById("company-list");
-    for (var i = 0; i < companies.length; i++) {
-      var name = companies[i].name;
-      var li = document.createElement("li");
-      var input = document.createElement("input");
-      input.type = "text";
-      input.value = name;
-      li.appendChild(input);
-      companyList.appendChild(li);
+    download: true,
+    header: true,
+    delimiter: ",",
+    dynamicTyping: true,
+    complete: function(results) {
+        const allCompanies = results.data;
+        let visibleCompanies = allCompanies.slice(0, 100); // первые 100 компаний
+        let nextIndex = 100; // индекс следующей компании, которая должна быть загружена
+
+        const companyList = document.getElementById("company-list");
+        const inputList = document.createElement("div");
+        companyList.appendChild(inputList);
+
+        visibleCompanies.forEach(company => {
+            const inn = company.inn;
+            const li = document.createElement("li");
+            const input = document.createElement("input");
+            input.type = "text";
+            input.value = inn;
+            li.appendChild(input);
+            inputList.appendChild(li);
+        });
+
+        window.addEventListener("scroll", handleScroll);
+
+        function handleScroll() {
+            const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+            // Проверка, достигли ли мы конца списка компаний
+            if (scrollTop + clientHeight >= scrollHeight && nextIndex < allCompanies.length) {
+                // Загрузка следующей порции компаний
+                const endIndex = nextIndex + 100;
+                const newCompanies = allCompanies.slice(nextIndex, endIndex);
+                nextIndex = endIndex;
+                visibleCompanies = [...visibleCompanies, ...newCompanies];
+
+                newCompanies.forEach(company => {
+                    const inn = company.inn;
+                    const li = document.createElement("li");
+                    const input = document.createElement("input");
+                    input.type = "text";
+                    input.value = inn;
+                    li.appendChild(input);
+                    inputList.appendChild(li);
+                });
+            }
+        }
     }
-  }
 });
